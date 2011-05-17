29fe12015b2f69715bd44dcfd76f96348c316ead
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2011-05-17 16:59:00 +0000

    Altered to run in 42X

--- test/Gen_Sim_Digi_Phase1_HCal_cfg.py
+++ test/Gen_Sim_Digi_Phase1_HCal_cfg.py
@@ -5,24 +5,25 @@ process = cms.Process("GenSimDigi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("Configuration.StandardSequences.Services_cff")
 
-#process.load('Configuration.StandardSequences.MixingNoPileUp_cff')
-process.load('SLHCUpgradeSimulations.Configuration.mixLowLumPU_HCal_P1Trk_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+#process.load('SLHCUpgradeSimulations.Configuration.mixLowLumPU_HCal_P1Trk_cff')
 #process.load('Configuration.StandardSequences.GeometryExtended_cff')
-process.load('SLHCUpgradeSimulations.Geometry.PhaseI_cmsSimIdealGeometryXML_R39F16_cff')
+process.load('SLHCUpgradeSimulations.Geometry.Phase1_R39F16_cmsSimIdealGeometryXML_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
-process.load("Configuration.StandardSequences.VtxSmearedBetafuncNominalCollision_cff")
+process.load('IOMC.EventVertexGenerators.VtxSmearedGauss_cfi')
 
 process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
 
 process.load("Configuration.StandardSequences.Sim_cff")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 #process.load("Configuration.StandardSequences.Digi_cff")
-process.load('SLHCUpgradeSimulations.Geometry.Digi_Phase1_cff')
+process.load('SLHCUpgradeSimulations.Geometry.Digi_Phase1_R39F16_cff')
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.load("Configuration.StandardSequences.FakeConditions_cff")
-process.GlobalTag.globaltag = 'DESIGN_36_V10::All'
+process.GlobalTag.globaltag = 'DESIGN42_V11::All'
+process.load("SLHCUpgradeSimulations.Geometry.fakeConditions_Phase1_R39F16_cff")
 
 # use hardcoded values
 process.es_hardcode.toGet.extend(['Gains', 'Pedestals', 'PedestalWidths', 'QIEData', 'ElectronicsMap','ChannelQuality','RespCorrs','ZSThresholds','LutMetadata','L1TriggerObjects','TimeCorrs','PFCorrs','LUTCorrs'])
@@ -96,14 +97,14 @@ process.simHcalDigis.HOlevel = -1000
 #turn on SiPMs in HO
 process.hcalSimParameters.ho.siPMCode = 1
 process.hcalSimParameters.ho.pixels = cms.int32(2500)
-process.hcalSimParameters.ho.photoelectronsToAnalog = 3.0
+process.hcalSimParameters.ho.photoelectronsToAnalog = cms.vdouble(3.0)
 
 #turn on SiPMs in HB/HE
 process.hcalSimParameters.hb.siPMCells = [1]
 process.hcalSimParameters.hb.pixels = cms.int32(4500*4)
-process.hcalSimParameters.hb.photoelectronsToAnalog = 10.0
+process.hcalSimParameters.hb.photoelectronsToAnalog = cms.vdouble(10.0)
 process.hcalSimParameters.he.pixels = cms.int32(4500*4)
-process.hcalSimParameters.he.photoelectronsToAnalog = 10.0
+process.hcalSimParameters.he.photoelectronsToAnalog = cms.vdouble(10.0)
 
 #turn on SLHC topology
 process.HcalTopologyIdealEP.SLHCMode = cms.untracked.bool(True)
